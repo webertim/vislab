b30a4965573f6e49dbf0710da4446a046dab3fee
webertim <weber.tim@hotmail.com>
2020-10-27 14:32:58 +0100

    Created exercise.md to answer the questions. Also added and image of the architecture and a dumpfile of the communication during the tests

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 import javax.xml.ws.Endpoint;
 
 import de.hska.iwi.vislab.lab1.example.ws.BuecherServiceImpl;
+import de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceImpl;
 
 
 /** Testserver fuer den Webservice */
@@ -11,7 +12,7 @@ public class TestWsServer {
 	public static void main(final String[] args) {
 		String url = (args.length > 0) ? args[0]
 				: "http://localhost:4434/buecherservice";
-		Endpoint ep = Endpoint.publish(url, new BuecherServiceImpl());
+		Endpoint ep = Endpoint.publish(url, new FibonacciServiceImpl());
 		JOptionPane.showMessageDialog(null, "TestWsServer beenden");
 		ep.stop();
 	}
