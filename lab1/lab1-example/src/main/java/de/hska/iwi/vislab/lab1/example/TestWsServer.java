f5c26902348ea430325bbb1d9d77bf580072df0e
Janek <janek9527@gmail.com>
2020-11-04 14:32:04 +0100

    add fibonnaciservice endpoint under correct url

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
@@ -12,8 +12,10 @@ public class TestWsServer {
 	public static void main(final String[] args) {
 		String url = (args.length > 0) ? args[0]
 				: "http://localhost:4434/buecherservice";
-		Endpoint ep = Endpoint.publish(url, new FibonacciServiceImpl());
+		Endpoint ep = Endpoint.publish(url, new BuecherServiceImpl());
+		Endpoint ep2 = Endpoint.publish("http://localhost:4434/fibonacciservice", new FibonacciServiceImpl());
 		JOptionPane.showMessageDialog(null, "TestWsServer beenden");
 		ep.stop();
+		ep2.stop();
 	}
 }
